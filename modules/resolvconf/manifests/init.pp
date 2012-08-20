b42c15c4b0af96fe6243d2951910a304a7a6d5bb
Todd A. Jacobs <github.projects@codegnome.org>
2012-08-19 20:57:45 -0400

    Use chaining to simplify resolvconf module.

--- modules/resolvconf/manifests/init.pp
+++ modules/resolvconf/manifests/init.pp
@@ -1,8 +1,5 @@
 class resolvconf {
-    $packages = ['resolvconf', 'dnsmasq']
-    package { $packages: ensure => installed, }
-
-    file { '/etc/resolvconf/resolv.conf.d/base':
+    package { 'resolvconf': } -> file { '/etc/resolvconf/resolv.conf.d/base':
         ensure  => file,
         owner   => root,
         group   => root,
@@ -10,11 +7,6 @@ class resolvconf {
 nameserver 127.0.0.1
 nameserver 8.8.8.8
 nameserver 8.8.4.4',
-    }
-
-    service { 'dnsmasq':
-        require   => Package[resolvconf],
-        ensure    => running,
-        subscribe => File['/etc/resolvconf/resolv.conf.d/base']
-    }
+        require => Package['resolvconf'],
+    } -> package { 'dnsmasq': }
 }
