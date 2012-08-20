87791c0677be08d9ae77fcab2444ec5da28bd02a
Todd A. Jacobs <github.projects@codegnome.org>
2012-08-19 22:06:05 -0400

    Subscribe apt-update to sources.list file.
    - Create some dependencies in file creation order to ensure that sources
      are created after the pinning file. This may not strictly be
      necessary.
    - Convert exec from a dependency to a subscription.
    - Trigger when APT sources change.

--- modules/apt_config/manifests/init.pp
+++ modules/apt_config/manifests/init.pp
@@ -11,14 +11,18 @@ class apt_config {
         '/etc/apt/apt.conf':
             ensure => present,
             source => "puppet:///modules/${module_name}/apt.conf";
-        '/etc/apt/sources.list':
-            ensure => present,
-            source => "puppet:///modules/${module_name}/sources.list";
         '/etc/apt/preferences.d/prefer_stable':
             ensure  => present,
             source  => "puppet:///modules/${module_name}/prefer_stable";
-    } -> exec { 'apt-update':
+        '/etc/apt/sources.list':
+            ensure => present,
+            source => "puppet:///modules/${module_name}/sources.list",
+            require => File['/etc/apt/preferences.d/prefer_stable'];
+    }
+
+    exec { 'apt-update':
         command => '/usr/bin/apt-get update',
         user => root,
+        subscribe => File['/etc/apt/sources.list'],
     }
 }
