a30e0cc3c0d1f0d68166d06035871286f3da7423
Todd A. Jacobs <github.projects@codegnome.org>
2012-08-22 18:24:19 -0400

    Enable host sound differently on Linux/Mac.
    - Enable bidirectional clipboard.
    - Detect host OS, and use correct sound device.
    - Install pulseaudio.
    - Add pulseaudio to ~vagrant/.bashrc on virtual machine.

--- lib/enable_sound_and_clipboard.sh
+++ lib/enable_sound_and_clipboard.sh
@@ -7,8 +7,21 @@ if [ ! -r .vagrant ]; then
     exit 72
 fi
 
+case `uname -s` in
+    Darwin)
+	audio='coreaudio'
+	;;
+    Linux)
+	audio='pulse'
+	;;
+esac
+
 vagrant halt
 VBoxManage modifyvm $(egrep -o '[[:xdigit:]]{8}-[^"]+' .vagrant) \
-    --audio coreaudio \
+    --audio "$audio" \
     --clipboard bidirectional
 vagrant up
+
+vagrant ssh -c 'sudo aptitude --assume-yes install pulseaudio'
+vagrant ssh -c \
+    'echo "pulseaudio --check || pulseaudio --daemonize" >> "$HOME/.bashrc"'
